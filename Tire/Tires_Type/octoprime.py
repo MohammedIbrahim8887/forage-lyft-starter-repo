ef571b26cc59a843745e7f5a1d41507577b82977
MohammedIbrahim8887 <agentmame@gmail.com>
2023-04-15 01:03:50 +0300

    tire class done

---
+++ Tire/Tires_Type/octoprime.py
@@ -0,0 +1,10 @@
+from Tire import tire
+
+
+class octoprime(tire):
+    def __init__(self, tire_wear):
+        self.tire_wear = tire_wear
+        self.tire_need_to_service = 3.0
+
+    def needs_service(self):
+        return sum(self.tire_wear) >= self.tire_need_to_service
